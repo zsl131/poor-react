c2e9ac378e487e8c525467b0271a18be286a8d98
zsl131 <398986099@qq.com>
2019-07-09 00:26:17 +0800

    修改单位

--- src/pages/web/personal/components/WebPersonalIndustry.js
+++ src/pages/web/personal/components/WebPersonalIndustry.js
@@ -11,7 +11,7 @@ export default class WebPersonalIndustry extends React.Component {
 
     return(
       <div className={styles.singleBlock}>
-        <p className={styles.header}><h3><Icon className={styles.icon} type="wallet"/> {personal.xm}家庭产业情况<span className="boldBlue">（三块地合计：{family.zjd + family.ld + family.gd} 亩）</span></h3></p>
+        <p className={styles.header}><h3><Icon className={styles.icon} type="wallet"/> {personal.xm}家庭产业情况<span className="boldBlue">（三块地合计：{family.ld + family.gd} 亩 + {family.zjd} 平）</span></h3></p>
         <table>
           <tbody>
           {/*<tr>
@@ -20,7 +20,7 @@ export default class WebPersonalIndustry extends React.Component {
           </tr>*/}
           <tr>
             <td className={styles.label}>宅基地</td>
-            <td className={styles.value}>{family.zjd} 亩</td>
+            <td className={styles.value}>{family.zjd} 平</td>
 
             <td className={styles.label}>林地</td>
             <td className={styles.value}>{family.ld} 亩</td>
